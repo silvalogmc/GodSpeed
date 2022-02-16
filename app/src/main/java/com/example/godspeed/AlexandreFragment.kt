208312610c2b5175d6dbbe9816c48c45cb8e3849
silvalogmc <silvalogmc@gmail.com>
2022-02-16 11:46:09 +0000

    fix team screen and start to add buttons

--- app/src/main/java/com/example/godspeed/AlexandreFragment.kt
+++ app/src/main/java/com/example/godspeed/AlexandreFragment.kt
@@ -1,11 +1,12 @@
-package com.example.godspeed.fragments
+package com.example.godspeed
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.godspeed.R
+import android.widget.ImageButton
+import com.example.godspeed.fragments.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -14,21 +15,23 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [TeamFragment.newInstance] factory method to
+ * Use the [AlexandreFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class TeamFragment : Fragment() {
+class AlexandreFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val btnAlexandre: ImageButton = R.id.btn_alexandre
+
+
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
-
     }
 
     override fun onCreateView(
@@ -36,7 +39,7 @@ class TeamFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_team, container, false)
+        return inflater.inflate(R.layout.fragment_alexandre, container, false)
     }
 
     companion object {
@@ -46,16 +49,22 @@ class TeamFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment TeamFragment.
+         * @return A new instance of fragment AlexandreFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            TeamFragment().apply {
+            AlexandreFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, fragment)
+        transaction.commit()
     }
 }
